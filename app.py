e4c0b8625ccc03cd4f76a622e28ad22fff91477b
aashif <khanayan070@gmail.com>
2025-10-03 02:59:30 +0530

    Gemini API Call

---
+++ app.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os 
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-pro")
+
+def get_gemini_response(question):
+    response =  model.generate_content(question)
+    return response.text
+
+st.set_page_config(page_title="Demo")
+
+st.header("Gemini Application")
+
+question = st.text_input("Enter your question:", key="inpur")
+
+submit = st.button("Submit")
+
+## When submit is click 
+
+if submit:
+    response = get_gemini_response(question)
+    st.subheader(" The Response is :")
+    st.write(response)
+
+
