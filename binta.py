8e1292f47844f0613597ff4f9785e8668616c572
aashif <khanayan070@gmail.com>
2026-02-08 00:46:19 +0530

    Change page title from 'My Feelings' to 'My Love'

--- binta.py
+++ binta.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="My Feelings ❤️",
+    page_title="My Love ❤️",
     page_icon="❤️",
     layout="centered"
 )
